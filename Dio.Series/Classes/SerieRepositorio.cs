b94480523480e8cf2cf38fe1f883047898167997
Paulo Rodrigues <82729606+adspaulorodrigues@users.noreply.github.com>
2021-10-06 23:20:59 -0300

    Add files via upload
    Criando um APP simples de cadastro de séries em .NET
    Bootcamp: Decola Tech

---
+++ Dio.Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        // Varivel tipo lista que vai conter todas as séries.
+        private List<Serie> listaSerie = new List<Serie>();
+        // Passa um objeto para o indice X do Vetor.
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir(); 
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+
+        
+    }
+}
